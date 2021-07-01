41a090405cc9eb8082554757960a4d64ec19230d
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2021-07-01 23:08:34 +0200

    Initial commit

---
+++ src/main/java/SurahNumber.java
@@ -0,0 +1,51 @@
+public enum ss {
+    s78("An_Nabaa"),
+    s79("An_Naziaat"),
+    s124("Aabasa"),
+    s81("At_Takwir"),
+    s82("Al_Infitar"),
+    s83("Al_Mutaffifin"),
+    s84("Al_Inshiqaq"),
+    s85("Al_Buruj"),
+    s86("At_Tariq"),
+    s87("Al_Aala"),
+    s88("Al_Ghashiyah"),
+    s89("Al_Fajr"),
+    s90("Al_Balad"),
+    s91("Ash_Shams"),
+    s92("Al_Layl"),
+    s93("Adh_Dhuha"),
+    s94("Al_Inshirah"),
+    s95("At_Tin"),
+    s96("Al_Aalaq"),
+    s97("Al_Qadar"),
+    s98("Al_Bayinah"),
+    s99("Az_Zalzalah("),
+    s100("Al_Aadiyah"),
+    s101("Al_Qariaah"),
+    s102("At_Takathur"),
+    s103("Al_Aasr"),
+    s104("Al_Humazah"),
+    s105("Al_Fil"),
+    s106("Quraish"),
+    s107("Al_Maaun"),
+    s108("Al_Kauthar"),
+    s109("Al_Kafirun"),
+    s110("An_Nasr"),
+    s111("Al_Masad"),
+    s112("Al_Ikhlas"),
+    s113("Al_Falaq"),
+    s114("An_Nas");
+
+    private final String name;
+
+    ss(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
