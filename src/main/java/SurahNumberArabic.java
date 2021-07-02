456019ccf9c3c3ff0ab3b74968feae74d940a276
ahmedhosnypro <80482137+ahmedhosnypro@users.noreply.github.com>
2021-07-02 02:02:21 +0200

    first work

--- src/main/java/SurahNumberArabic.java
+++ src/main/java/SurahNumberArabic.java
@@ -1,2 +1,313 @@
-package PACKAGE_NAME;public enum SurahNumberArabic {
+public enum SurahNumberArabic {
+    s78("النبأ", new String[]{"https://t.me/wahidbaly_tafseerNateq/37",
+            "https://t.me/wahidbaly_tafseerNateq/41"}),
+
+    s79("النازعات", new String[]{"https://t.me/wahidbaly_tafseerNateq/42",
+            "https://t.me/wahidbaly_tafseerNateq/43",
+            "https://t.me/wahidbaly_tafseerNateq/45"}),
+
+    s80("عبس", new String[]{"https://t.me/wahidbaly_tafseerNateq/46",
+            "https://t.me/wahidbaly_tafseerNateq/49",
+            "https://t.me/wahidbaly_tafseerNateq/50"}),
+
+    s81("التكوير", new String[]{"https://t.me/wahidbaly_tafseerNateq/53",
+            "https://t.me/wahidbaly_tafseerNateq/54",
+            "https://t.me/wahidbaly_tafseerNateq/55"}),
+
+    s82("الانفطار", new String[]{"https://t.me/wahidbaly_tafseerNateq/57",
+            "https://t.me/wahidbaly_tafseerNateq/58",
+            "https://t.me/wahidbaly_tafseerNateq/60"}),
+
+    s83("المطفّفين", new String[]{"https://t.me/wahidbaly_tafseerNateq/61",
+            "https://t.me/wahidbaly_tafseerNateq/64",
+            "https://t.me/wahidbaly_tafseerNateq/63",}),
+
+    s84("الانشقاق", new String[]{"https://t.me/wahidbaly_tafseerNateq/69",
+            "https://t.me/wahidbaly_tafseerNateq/67",
+            "https://t.me/wahidbaly_tafseerNateq/68"}),
+
+    s85("البروج", new String[]{"https://t.me/wahidbaly_tafseerNateq/73",
+            "https://t.me/wahidbaly_tafseerNateq/72",
+            "https://t.me/wahidbaly_tafseerNateq/71"}),
+
+    s86("الطارق", new String[]{"https://t.me/wahidbaly_tafseerNateq/77",
+            "https://t.me/wahidbaly_tafseerNateq/76",
+            "https://t.me/wahidbaly_tafseerNateq/75"}),
+
+    s87("الأعلى", new String[]{"https://t.me/wahidbaly_tafseerNateq/79",
+            "https://t.me/wahidbaly_tafseerNateq/80",
+            "https://t.me/wahidbaly_tafseerNateq/81"}),
+
+    s88("الغاشية", new String[]{"https://t.me/wahidbaly_tafseerNateq/83",
+            "https://t.me/wahidbaly_tafseerNateq/84",
+            "https://t.me/wahidbaly_tafseerNateq/85"}),
+
+    s89("الفجر", new String[]{"https://t.me/wahidbaly_tafseerNateq/87",
+            "https://t.me/wahidbaly_tafseerNateq/88",
+            "https://t.me/wahidbaly_tafseerNateq/89"}),
+
+    s90("البلد", new String[]{"https://t.me/wahidbaly_tafseerNateq/91",
+            "https://t.me/wahidbaly_tafseerNateq/92",
+            "https://t.me/wahidbaly_tafseerNateq/94"}),
+
+    s91("الشمس", new String[]{"https://t.me/wahidbaly_tafseerNateq/96",
+            "https://t.me/wahidbaly_tafseerNateq/97",
+            "https://t.me/wahidbaly_tafseerNateq/98"}),
+
+    s92("الليل", new String[]{"https://t.me/wahidbaly_tafseerNateq/100",
+            "https://t.me/wahidbaly_tafseerNateq/101",
+            "https://t.me/wahidbaly_tafseerNateq/102"}),
+
+    s93("الضحى", new String[]{"https://t.me/wahidbaly_tafseerNateq/104",
+            "https://t.me/wahidbaly_tafseerNateq/105",
+            "https://t.me/wahidbaly_tafseerNateq/106"}),
+
+    s94("الشرح", new String[]{"https://t.me/wahidbaly_tafseerNateq/108",
+            "https://t.me/wahidbaly_tafseerNateq/109",
+            "https://t.me/wahidbaly_tafseerNateq/110"}),
+
+    s95("التين", new String[]{"https://t.me/wahidbaly_tafseerNateq/112",
+            "https://t.me/wahidbaly_tafseerNateq/113",
+            "https://t.me/wahidbaly_tafseerNateq/114"}),
+
+    s96("العلق", new String[]{"https://t.me/wahidbaly_tafseerNateq/116",
+            "https://t.me/wahidbaly_tafseerNateq/117",
+            "https://t.me/wahidbaly_tafseerNateq/118"}),
+
+    s97("القدر", new String[]{"https://t.me/wahidbaly_tafseerNateq/120",
+            "https://t.me/wahidbaly_tafseerNateq/121",
+            "https://t.me/wahidbaly_tafseerNateq/122"}),
+
+    s98("البينة", new String[]{"https://t.me/wahidbaly_tafseerNateq/124",
+            "https://t.me/wahidbaly_tafseerNateq/125",
+            "https://t.me/wahidbaly_tafseerNateq/126"}),
+
+    s99("الزلزلة", new String[]{"https://t.me/wahidbaly_tafseerNateq/128",
+            "https://t.me/wahidbaly_tafseerNateq/129",
+            "https://t.me/wahidbaly_tafseerNateq/130"}),
+
+    s100("العاديات", new String[]{"https://t.me/wahidbaly_tafseerNateq/132",
+            "https://t.me/wahidbaly_tafseerNateq/133",
+            "https://t.me/wahidbaly_tafseerNateq/134"}),
+
+    s101("القارعة", new String[] {"https://t.me/wahidbaly_tafseerNateq/136",
+            "https://t.me/wahidbaly_tafseerNateq/137",
+            "https://t.me/wahidbaly_tafseerNateq/138"}),
+
+    s102("التكاثر", new String[]{"https://t.me/wahidbaly_tafseerNateq/140",
+            "https://t.me/wahidbaly_tafseerNateq/141",
+            "https://t.me/wahidbaly_tafseerNateq/142"}),
+
+    s103("العصر", new String[]{"https://t.me/wahidbaly_tafseerNateq/144",
+            "https://t.me/wahidbaly_tafseerNateq/145",
+            "https://t.me/wahidbaly_tafseerNateq/146"}),
+
+    s104("الهمزة", new String[]{"https://t.me/wahidbaly_tafseerNateq/148",
+            "https://t.me/wahidbaly_tafseerNateq/149",
+            "https://t.me/wahidbaly_tafseerNateq/150"}),
+
+    s105("الفيل", new String[]{"https://t.me/wahidbaly_tafseerNateq/152",
+            "https://t.me/wahidbaly_tafseerNateq/178",
+            "https://t.me/wahidbaly_tafseerNateq/177"}),
+
+    s106("قريش", new String[]{"https://t.me/wahidbaly_tafseerNateq/154",
+            "https://t.me/wahidbaly_tafseerNateq/155",
+            "https://t.me/wahidbaly_tafseerNateq/156"}),
+
+    s107("الماعون", new String[]{"https://t.me/wahidbaly_tafseerNateq/158",
+            "https://t.me/wahidbaly_tafseerNateq/159",
+            "https://t.me/wahidbaly_tafseerNateq/160"}),
+
+    s108("الكوثر", new String[]{"https://t.me/wahidbaly_tafseerNateq/162",
+            "https://t.me/wahidbaly_tafseerNateq/163",
+            "https://t.me/wahidbaly_tafseerNateq/164"}),
+
+    s109("الكافرون", new String[]{"https://t.me/wahidbaly_tafseerNateq/166",
+            "https://t.me/wahidbaly_tafseerNateq/167",
+            "https://t.me/wahidbaly_tafseerNateq/168"}),
+
+    s110("النصر", new String[]{"https://t.me/wahidbaly_tafseerNateq/170",
+            "https://t.me/wahidbaly_tafseerNateq/171",
+            "https://t.me/wahidbaly_tafseerNateq/172"}),
+
+    s111("المسد", new String[]{"https://t.me/wahidbaly_tafseerNateq/174",
+            "https://t.me/wahidbaly_tafseerNateq/175",
+            "https://t.me/wahidbaly_tafseerNateq/176"}),
+
+    s112("الإخلاص", new String[]{"https://t.me/wahidbaly_tafseerNateq/180",
+            "https://t.me/wahidbaly_tafseerNateq/181",
+            "https://t.me/wahidbaly_tafseerNateq/182"}),
+
+    s113("الفلق", new String[]{"https://t.me/wahidbaly_tafseerNateq/184",
+            "https://t.me/wahidbaly_tafseerNateq/185",
+            "https://t.me/wahidbaly_tafseerNateq/186"}),
+
+    s114("النّاس", new String[]{"https://t.me/wahidbaly_tafseerNateq/188",
+            "https://t.me/wahidbaly_tafseerNateq/189",
+            "https://t.me/wahidbaly_tafseerNateq/190"});
+
+    private final String name;
+
+    private final String[] content;
+
+    SurahNumberArabic(String name, String[] content) {
+        this.name = name;
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String[] getContent() {
+        return content;
+    }
+
+    static private final String[] An_Nabaa = {"https://t.me/wahidbaly_tafseerNateq/37",
+            "https://t.me/wahidbaly_tafseerNateq/41"};
+
+
+    static private final String[] An_Naziaat = {"https://t.me/wahidbaly_tafseerNateq/42",
+            "https://t.me/wahidbaly_tafseerNateq/43",
+            "https://t.me/wahidbaly_tafseerNateq/45"};
+
+    static private final String[] Aabasa = {"https://t.me/wahidbaly_tafseerNateq/46",
+            "https://t.me/wahidbaly_tafseerNateq/49",
+            "https://t.me/wahidbaly_tafseerNateq/50"};
+
+    static private final String[] At_Takwir = {"https://t.me/wahidbaly_tafseerNateq/53",
+            "https://t.me/wahidbaly_tafseerNateq/54",
+            "https://t.me/wahidbaly_tafseerNateq/55"};
+
+    static private final String[] Al_Infitar = {"https://t.me/wahidbaly_tafseerNateq/57",
+            "https://t.me/wahidbaly_tafseerNateq/58",
+            "https://t.me/wahidbaly_tafseerNateq/60"};
+
+    static private final String[] Al_Mutaffifin = {"https://t.me/wahidbaly_tafseerNateq/61",
+            "https://t.me/wahidbaly_tafseerNateq/64",
+            "https://t.me/wahidbaly_tafseerNateq/63",};
+
+    static private final String[] Al_Inshiqaq = {"https://t.me/wahidbaly_tafseerNateq/69",
+            "https://t.me/wahidbaly_tafseerNateq/67",
+            "https://t.me/wahidbaly_tafseerNateq/68"};
+
+    static private final String[] Al_Buruj = {"https://t.me/wahidbaly_tafseerNateq/73",
+            "https://t.me/wahidbaly_tafseerNateq/72",
+            "https://t.me/wahidbaly_tafseerNateq/71"};
+
+    static private final String[] At_Tariq = {"https://t.me/wahidbaly_tafseerNateq/77",
+            "https://t.me/wahidbaly_tafseerNateq/76",
+            "https://t.me/wahidbaly_tafseerNateq/75"};
+
+    static private final String[] Al_Aala = {"https://t.me/wahidbaly_tafseerNateq/79",
+            "https://t.me/wahidbaly_tafseerNateq/80",
+            "https://t.me/wahidbaly_tafseerNateq/81"};
+
+    static private final String[] Al_Ghashiyah = {"https://t.me/wahidbaly_tafseerNateq/83",
+            "https://t.me/wahidbaly_tafseerNateq/84",
+            "https://t.me/wahidbaly_tafseerNateq/85"};
+
+    static private final String[] Al_Fajr = {"https://t.me/wahidbaly_tafseerNateq/87",
+            "https://t.me/wahidbaly_tafseerNateq/88",
+            "https://t.me/wahidbaly_tafseerNateq/89"};
+
+    static private final String[] Al_Balad = {"https://t.me/wahidbaly_tafseerNateq/91",
+            "https://t.me/wahidbaly_tafseerNateq/92",
+            "https://t.me/wahidbaly_tafseerNateq/94"};
+
+    static private final String[] Ash_Shams = {"https://t.me/wahidbaly_tafseerNateq/96",
+            "https://t.me/wahidbaly_tafseerNateq/97",
+            "https://t.me/wahidbaly_tafseerNateq/98"};
+
+    static private final String[] Al_Layl = {"https://t.me/wahidbaly_tafseerNateq/100",
+            "https://t.me/wahidbaly_tafseerNateq/101",
+            "https://t.me/wahidbaly_tafseerNateq/102"};
+
+    static private final String[] Adh_Dhuha = {"https://t.me/wahidbaly_tafseerNateq/104",
+            "https://t.me/wahidbaly_tafseerNateq/105",
+            "https://t.me/wahidbaly_tafseerNateq/106"};
+
+    static private final String[] Al_Inshirah = {"https://t.me/wahidbaly_tafseerNateq/108",
+            "https://t.me/wahidbaly_tafseerNateq/109",
+            "https://t.me/wahidbaly_tafseerNateq/110"};
+
+    static private final String[] At_Tin = {"https://t.me/wahidbaly_tafseerNateq/112",
+            "https://t.me/wahidbaly_tafseerNateq/113",
+            "https://t.me/wahidbaly_tafseerNateq/114"};
+
+    static private final String[] Al_Aalaq = {"https://t.me/wahidbaly_tafseerNateq/116",
+            "https://t.me/wahidbaly_tafseerNateq/117",
+            "https://t.me/wahidbaly_tafseerNateq/118"};
+
+    static private final String[] Al_Qadar = {"https://t.me/wahidbaly_tafseerNateq/120",
+            "https://t.me/wahidbaly_tafseerNateq/121",
+            "https://t.me/wahidbaly_tafseerNateq/122"};
+
+    static private final String[] Al_Bayinah = {"https://t.me/wahidbaly_tafseerNateq/124",
+            "https://t.me/wahidbaly_tafseerNateq/125",
+            "https://t.me/wahidbaly_tafseerNateq/126"};
+
+    static private final String[] Az_Zalzalah = {"https://t.me/wahidbaly_tafseerNateq/128",
+            "https://t.me/wahidbaly_tafseerNateq/129",
+            "https://t.me/wahidbaly_tafseerNateq/130"};
+
+    static private final String[] Al_Aadiyah = {"https://t.me/wahidbaly_tafseerNateq/132",
+            "https://t.me/wahidbaly_tafseerNateq/133",
+            "https://t.me/wahidbaly_tafseerNateq/134"};
+
+    static private final String[] Al_Qariaah = {"https://t.me/wahidbaly_tafseerNateq/136",
+            "https://t.me/wahidbaly_tafseerNateq/137",
+            "https://t.me/wahidbaly_tafseerNateq/138"};
+
+    static private final String[] At_Takathur = {"https://t.me/wahidbaly_tafseerNateq/140",
+            "https://t.me/wahidbaly_tafseerNateq/141",
+            "https://t.me/wahidbaly_tafseerNateq/142"};
+
+    static private final String[] Al_Aasr = {"https://t.me/wahidbaly_tafseerNateq/144",
+            "https://t.me/wahidbaly_tafseerNateq/145",
+            "https://t.me/wahidbaly_tafseerNateq/146"};
+
+    static private final String[] Al_Humazah = {"https://t.me/wahidbaly_tafseerNateq/148",
+            "https://t.me/wahidbaly_tafseerNateq/149",
+            "https://t.me/wahidbaly_tafseerNateq/150"};
+
+    static private final String[] Al_Fil = {"https://t.me/wahidbaly_tafseerNateq/152",
+            "https://t.me/wahidbaly_tafseerNateq/178",
+            "https://t.me/wahidbaly_tafseerNateq/177"};
+
+    static private final String[] Quraish = {"https://t.me/wahidbaly_tafseerNateq/154",
+            "https://t.me/wahidbaly_tafseerNateq/155",
+            "https://t.me/wahidbaly_tafseerNateq/156"};
+
+    static private final String[] Al_Maaun = {"https://t.me/wahidbaly_tafseerNateq/158",
+            "https://t.me/wahidbaly_tafseerNateq/159",
+            "https://t.me/wahidbaly_tafseerNateq/160"};
+
+    static private final String[] Al_Kauthar = {"https://t.me/wahidbaly_tafseerNateq/162",
+            "https://t.me/wahidbaly_tafseerNateq/163",
+            "https://t.me/wahidbaly_tafseerNateq/164"};
+
+    static private final String[] Al_Kafirun = {"https://t.me/wahidbaly_tafseerNateq/166",
+            "https://t.me/wahidbaly_tafseerNateq/167",
+            "https://t.me/wahidbaly_tafseerNateq/168"};
+
+    static private final String[] An_Nasr = {"https://t.me/wahidbaly_tafseerNateq/170",
+            "https://t.me/wahidbaly_tafseerNateq/171",
+            "https://t.me/wahidbaly_tafseerNateq/172"};
+
+    static private final String[] Al_Masad = {"https://t.me/wahidbaly_tafseerNateq/174",
+            "https://t.me/wahidbaly_tafseerNateq/175",
+            "https://t.me/wahidbaly_tafseerNateq/176"};
+
+    static private final String[] Al_Ikhlas = {"https://t.me/wahidbaly_tafseerNateq/180",
+            "https://t.me/wahidbaly_tafseerNateq/181",
+            "https://t.me/wahidbaly_tafseerNateq/182"};
+
+    static private final String[] Al_Falaq = {"https://t.me/wahidbaly_tafseerNateq/184",
+            "https://t.me/wahidbaly_tafseerNateq/185",
+            "https://t.me/wahidbaly_tafseerNateq/186"};
+
+    static private final String[] An_Nas = {"https://t.me/wahidbaly_tafseerNateq/188",
+            "https://t.me/wahidbaly_tafseerNateq/189",
+            "https://t.me/wahidbaly_tafseerNateq/190"};
 }
